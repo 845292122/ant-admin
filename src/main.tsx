1b50be38f40f069d8eb04d98aa0ed3febe63a161
edison <845292122@qq.com>
2025-01-27 21:53:58 +0800

    fix: 去除unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import 'virtual:uno.css'
 import Router from './router'
 import AuthRouter from './router/helper/authRouter'
 import { ConfigProvider, ThemeConfig } from 'antd'
