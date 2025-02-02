988e3804b2a90819354e4b5f473df82cf269d379
edison <845292122@qq.com>
2025-02-02 13:26:44 +0800

    feat: 页面

--- src/pages/system/Perm.tsx
+++ src/pages/system/Perm.tsx
@@ -1,3 +1,28 @@
+import { QueryFormField } from '~/components/QueryForm'
+
+const queryFormFields: QueryFormField[] = [
+  {
+    name: 'name',
+    label: '权限名称',
+    type: 'input'
+  },
+  {
+    name: 'status',
+    label: '状态',
+    type: 'select',
+    options: [
+      {
+        label: '启用',
+        value: 1
+      },
+      {
+        label: '停用',
+        value: 0
+      }
+    ]
+  }
+]
+
 const Perm: React.FC = () => {
   return (
     <div>
